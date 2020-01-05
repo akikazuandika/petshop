25ee834b4f7f6285560beec776332e4c52b9e2b7
Caca Wilda Andika <akikazuandika@gmail.com>
2020-01-06 02:06:02 +0700

    Fix register

--- app/src/main/java/com/appneko/petshop/RegisterActivity.kt
+++ app/src/main/java/com/appneko/petshop/RegisterActivity.kt
@@ -1,10 +1,14 @@
 package com.appneko.petshop
 
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.appneko.petsho.UsersModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_register.*
 import java.util.*
 import kotlin.concurrent.schedule
@@ -21,18 +25,33 @@ class RegisterActivity : AppCompatActivity() {
             var name = register_inp_name.text.toString()
             val db = MyDatabaseOpenHelper(this).writableDatabase
 
-            var query = "INSERT INTO ${UsersModel.TABLE_NAME}(${UsersModel.USERNAME}, ${UsersModel.PASSWORD}, ${UsersModel.NAME}, ${UsersModel.TYPE}) VALUES('${username}', '${password}', '${name}', 'user')"
-            var result = db.rawQuery(query, null)
-            if (result.moveToFirst()){
-                if (result.getString(0).toString() != "-1"){
-                    Toast.makeText(this, "Berhasil", Toast.LENGTH_LONG).show()
-                    Timer("Done", false).schedule(1000){
-                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }else{
-                    Toast.makeText(this, "Gagal", Toast.LENGTH_LONG).show()
+            var cv = ContentValues()
+
+            cv.put(UsersModel.NAME, name)
+            cv.put(UsersModel.USERNAME, username)
+            cv.put(UsersModel.PASSWORD, password)
+            cv.put(UsersModel.TYPE, "user")
+
+            var result = db.insert(UsersModel.TABLE_NAME, null, cv)
+
+            if (result == (-1).toLong()){
+                val inputManager: InputMethodManager =getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED)
+
+                var snackbar = Snackbar.make(it, "Failed", Snackbar.LENGTH_SHORT)
+                snackbar.show()
+            }else{
+                val inputManager: InputMethodManager =getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED)
+
+                var snackbar = Snackbar.make(it, "Sucess Register", Snackbar.LENGTH_SHORT)
+                snackbar.show()
+
+                var intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+
+                Timer("Done", false).schedule(1000){
+                    startActivity(intent)
+                    finish()
                 }
             }
         }
